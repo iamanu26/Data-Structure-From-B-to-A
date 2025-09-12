09ff003da23b23b99dfe34425f2e958a077a3181
Anurag Dubey <anuragdubey26202@gmail.com>
2025-09-12 18:44:20 +0530

    Add files via upload

---
+++ Arrays/problem_1_Two_Sum_Problem.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map <int , int > mpp;
+        int n = nums.size();
+        for(int i = 0 ; i<n ; i++)
+        {
+            int a = nums[i];
+            int moreNeeded = target -  a;
+            if(mpp.find(moreNeeded) != mpp.end() )
+            {
+                return{mpp[moreNeeded] , i};
+            }
+            mpp[a] = i;
+        }
+        return {-1,-1};
+    }
+};
